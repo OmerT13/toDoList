7133e8e6a42b04e34cca7fcac28cec95e22480a1
OT13 <omertahir13@gmail.com>
2021-02-03 08:10:21 +0800

    Created out Room Database abstract class and declared the database singleton

---
+++ app/src/main/java/com/ooteedemo/todo/data/ToDoRoomDatabase.java
@@ -0,0 +1,29 @@
+package com.ooteedemo.todo.data;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.ooteedemo.todo.model.ToDo;
+
+@Database(entities = {ToDo.class}, version = 1)
+public abstract class ToDoRoomDatabase extends RoomDatabase {
+
+//    DBINSTANCE is a singleton that we create to make sure we don't instantiate the DB more than once
+    private static volatile ToDoRoomDatabase DBINSTANCE;
+    public abstract ToDoDao toDoDao();
+
+    static ToDoRoomDatabase getDatabase(final Context context) {
+        if (DBINSTANCE==null) {
+            synchronized (ToDoRoomDatabase.class) {
+                if (DBINSTANCE==null) {
+                    DBINSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            ToDoRoomDatabase.class,"todo_db").build();
+                }
+            }
+        }
+        return DBINSTANCE;
+    }
+}
