5ef069cef7413621cf181709f48e6629917ae79a
OT13 <omertahir13@gmail.com>
2021-02-08 08:24:31 +0800

    Created the ToDo List Adapter

---
+++ app/src/main/java/com/ooteedemo/todo/ui/ToDoListAdapter.java
@@ -0,0 +1,61 @@
+package com.ooteedemo.todo.ui;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ooteedemo.todo.R;
+import com.ooteedemo.todo.model.ToDo;
+
+import java.util.List;
+
+public class ToDoListAdapter extends RecyclerView.Adapter<ToDoListAdapter.ViewHolder>{
+
+    private final LayoutInflater toDoInflater;
+    private List<ToDo> toDoList;
+
+    public ToDoListAdapter(Context context) {
+        toDoInflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = toDoInflater.inflate(R.layout.recyclerview_item,viewGroup,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ToDoListAdapter.ViewHolder viewHolder, int position) {
+        if (toDoList!=null) {
+            ToDo current = toDoList.get(position);
+            viewHolder.toDoTextView.setText(current.getNoDo());
+        } else {
+            viewHolder.toDoTextView.setText(R.string.no_todo);
+        }
+    }
+
+    void setToDos(List<ToDo> toDos) {
+        toDoList = toDos;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        if (toDoList!=null) return toDoList.size();
+        else return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView toDoTextView;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            toDoTextView = itemView.findViewById(R.id.textView);
+        }
+    }
+}
